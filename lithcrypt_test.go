d053595e07e18b29966d047935d3dfd83ec83c0f
Richard Lyman <richard.lyman@gmail.com>
2013-03-30 19:43:00 -0600

    .

---
+++ lithcrypt_test.go
@@ -0,0 +1,22 @@
+package lithcrypt
+
+import (
+	"testing"
+)
+
+var password = []byte("some password")
+var payload = []byte("some payload")
+
+func TestDecEnc( t *testing.T ){
+    encrypted_payload,_ := Encrypt(password, payload)
+    if p,_ := Decrypt(password, encrypted_payload); string(p) != string(payload) {
+        t.Error("Decrypting what was encrypted did not return the original payload")
+    }
+}
+
+func TestDecCustomEnc( t *testing.T ){
+    encrypted_payload,_ := ParameterizedEncrypt(password, payload, 2, 1, 1, 16)
+    if p,_ := Decrypt(password, encrypted_payload); string(p) != string(payload) {
+        t.Error("Decrypting what was custom encrypted did not return the original payload")
+    }
+}
